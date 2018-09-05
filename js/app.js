9c1b692aeae2276d5d0e708378f2567dfc967a71
Krishen Rana <krishenrana@me.com>
2018-09-05 11:44:23 -0600

    URLs Deployment (Test)

--- js/app.js
+++ js/app.js
@@ -5,36 +5,36 @@ angular.module("findMusicApp").config(function($locationProvider, $routeProvider
     $routeProvider.when("Krontime-Project1.github.io/project_1", {
         templateUrl: "Krontime-Project1.github.io/templates/home.html",
     })
-    .when("Krontime-Project1.github.io/copyright", {
+    .when("/copyright", {
         templateUrl: "Krontime-Project1.github.io/templates/copyright.html",
     })
-    .when("Krontime-Project1.github.io/contact",{
+    .when("/contact",{
         templateUrl: "Krontime-Project1.github.io/templates/contact.html"
     })
-    .when("Krontime-Project1.github.io/add", {
+    .when("/add", {
         templateUrl: "Krontime-Project1.github.io/templates/add.html",
         controller: "Post"
     })
-    .when("Krontime-Project1.github.io/accounts/register", {
+    .when("/accounts/register", {
         templateUrl: "Krontime-Project1.github.io/templates/register.html",
         controller: "Register"
     })
-    .when("Krontime-Project1.github.io/about", {
+    .when("/about", {
         templateUrl: "Krontime-Project1.github.io/templates/about.html",
     })
-    .when("Krontime-Project1.github.io/find_music", {
+    .when("/find_music", {
         templateUrl: "Krontime-Project1.github.io/templates/find_music.html",
         controller: "Events"
     })
-    .when("Krontime-Project1.github.io/find_artists/", {
+    .when("/find_artists/", {
         templateUrl: "Krontime-Project1.github.io/templates/find_artists.html",
         controller: "informationRouting"
     })
-    .when("Krontime-Project1.github.io/find_artists/:id", {
+    .when("/find_artists/:id", {
         templateUrl: "Krontime-Project1.github.io/templates/find_artists.html",
         controller: "informationContent"
     })
-    .when("Krontime-Project1.github.io/todo/edit/:id", {
+    .when("/todo/edit/:id", {
         templateUrl: "Krontime-Project1.github.io/templates/edit-todo.html",
         controller: "EditTodoController"
     });
